cadb6d42f7a4cf71c23fe870f1e38ce0cd5dc267
Pablo Rizo <prozasocon4@gmail.com>
2020-11-19 15:59:07 -0600

    COmmit para la version 3 del apk

--- utils/asignarForFirebase.js
+++ utils/asignarForFirebase.js
@@ -1,27 +1,26 @@
 export const parseFirebase = (res, setState) => {
-    let arr = [];
-  
-    arr = res.map((a) => {
-      let jsonfins = { id: "", data: {} };
-  
-      jsonfins.id = a.id;
-      delete a.id;
-      delete a._id;
-      jsonfins.data = a;
-  
-      return jsonfins;
-    });
-    setState(arr);
-  };
-  
-  export const parseFirebaseNorm = (res, setState) => {
+  let arr = [];
+
+  arr = res.map((a) => {
     let jsonfins = { id: "", data: {} };
-  
-    jsonfins.id = res.id;
-    delete res.id;
-    delete res._id;
-    jsonfins.data = res;
-  
-    setState(jsonfins);
-  };
-  
+
+    jsonfins.id = a.id;
+    delete a.id;
+    delete a._id;
+    jsonfins.data = a;
+
+    return jsonfins;
+  });
+  setState(arr);
+};
+
+export const parseFirebaseNorm = (res, setState) => {
+  let jsonfins = { id: "", data: {} };
+
+  jsonfins.id = res.id;
+  delete res.id;
+  delete res._id;
+  jsonfins.data = res;
+
+  setState(jsonfins);
+};
